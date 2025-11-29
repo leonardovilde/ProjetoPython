5f003c29a4d60fb0980727e818b61656aad51cab
leonardovilde <leonardovilde@gmail.com>
2025-11-29 11:24:32 -0300

    atualizando projeto

--- projetoPythonBancoDados/interacaoUsuario.py
+++ projetoPythonBancoDados/interacaoUsuario.py
@@ -8,8 +8,9 @@ def menu_interacao():
         continuarSistema = input("Deseja continuar ou sair\nDigite 1 para Continuar e 2 para Sair: ")
 
         if continuarSistema == "1":
-            print("Você Continuou!")
+            print("Bem vindo ao nosso sistema!")
             print("Verificando se existem tabelas no banco...\n")
+            print("Nenhuma tabela encontrada")
 
             
             criarTabela = input("Deseja criar uma tabela?\nDigite 1 para Sim e 2 para Não: ")
@@ -19,7 +20,7 @@ def menu_interacao():
                 print(f"Tabela '{nomeTabela}' criada com sucesso!\n")
 
             elif criarTabela == "2":
-                print("Nenhuma tabela será criada!\n")
+                print("A tabela não foi criada!\n")
 
             else:
                 print("Opção inválida!\n")
@@ -32,7 +33,7 @@ def menu_interacao():
                 print("Deletando tabelas...\n")
 
             elif deletarTabela == "2":
-                print("Nenhuma tabela será deletada!\n")
+                print("A Tabela não foi deletada!\n")
 
             else:
                 print("Opção inválida!\n")
@@ -43,8 +44,8 @@ def menu_interacao():
 
         elif continuarSistema == "2":
 
-            for animacao in range(10):
-                sleep(0.3)
+            for animacao in range(8):
+                sleep(0.2)
                 print("*")
 
             print("Você saiu do sistema.")
