8c2326f9daf5d70ee9d2e0548fa257370e457889
leonardovilde <leonardovilde@gmail.com>
2025-12-05 18:29:39 -0300

    atualização do projeto

--- projetoPythonBancoDados/crud.py
+++ projetoPythonBancoDados/crud.py
@@ -1,51 +1,29 @@
 import sqlite3
 
 def createTable():
-            criarTabela = input("Deseja criar uma tabela?\nDigite 1 para Sim e 2 para Não: ")
+      print('Criando tabela....')
 
-            if criarTabela == "1":
-                nomeTabela = input("Qual o nome da tabela que deseja criar? ")
-                print(f"Tabela '{nomeTabela}' criada com sucesso!\n")
+def readTable(conexao, cursor):
+            conexao = sqlite3.connect("ProjetoPython.sqlite")
+            cursor = conexao.cursor()
 
-            elif criarTabela == "2":
-                print("A tabela não foi criada!\n")
+            listarTabelas = "SELECT name FROM sqlite_master WHERE type='table';"
 
-            else:
-                print("Opção inválida!\n")
+            cursor.execute(listarTabelas)
 
-def readTable(cursor, conexao):
-     conexao = sqlite3.connect("ProjetoPython.sqlite")
-     cursor = conexao.cursor()
+            tabelas = cursor.fetchall()
 
-     listarTabelas = "SELECT name FROM sqlite_master WHERE type='table';"
+            if tabelas:
+             for tabela in tabelas:
+              print("Tabela", tabela[0])
 
-     cursor.execute(listarTabelas)
-
-     tabelas = cursor.fetchall()
-
-     if tabelas:
-      for tabela in tabelas:
-        print("Tabela", tabela[0])
-
-     else: 
-      print("nenhuma Tabela encontrada no banco atual")
+            else: 
+             print("nenhuma Tabela encontrada no banco atual")
 
 
 def update():
     pass
 
 def deleteTable():
-            deletarTabela = input("Deseja deletar uma tabela?\nDigite 1 para Sim e 2 para Não: ")
-
-            if deletarTabela == "1":
-                print("Deletando tabelas...\n")
-                print("Tabela deletada com sucesso")
-
-            elif deletarTabela == "2":
-                print("A Tabela não foi deletada!\n")
-
-            else:
-                print("Opção inválida!\n")
-
-            loop = True  
+    print("Deletando....")
 
--- projetoPythonBancoDados/interacaoUsuario.py
+++ projetoPythonBancoDados/interacaoUsuario.py
@@ -16,12 +16,38 @@ def menu_interacao():
             conexao = sqlite3.connect("ProjetoPython.sqlite")
             cursor = conexao.cursor()
 
-            readTable(cursor, conexao)
+            readTable(conexao, cursor)
+
+
+            criarTabela = input("Deseja criar uma tabela?\nDigite 1 para Sim e 2 para Não: ")
 
             createTable()
 
+            if criarTabela == "1":
+                nomeTabela = input("Qual o nome da tabela que deseja criar? ")
+                print(f"Tabela '{nomeTabela}' criada com sucesso!\n")
+
+            elif criarTabela == "2":
+                print("A tabela não foi criada!\n")
+
+            else:
+                print("Opção inválida!\n")
+
+            deletarTabela = input("Deseja deletar uma tabela?\nDigite 1 para Sim e 2 para Não: ")
+
             deleteTable()
-            
+
+            if deletarTabela == "1":
+                print("Tabela deletada com sucesso")
+
+            elif deletarTabela == "2":
+                print("A Tabela não foi deletada!\n")
+
+            else:
+                print("Opção inválida!\n")
+
+            loop = True  
+
 
         elif continuarSistema == "2":
 
